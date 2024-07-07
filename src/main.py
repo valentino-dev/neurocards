0c6ccf0b97b5f0f9d05fb56f4d7c37f101aee04c
valentino-dev <angelo.valentino.brade@gmail.com>
2024-07-07 03:37:13 +0200

    more testing

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import scipy
 import numpy as np
+import random
 from collections import deque
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
@@ -39,8 +40,10 @@ class DQNAgent:
         self.memory.append((state, action, reward, next_state, done))
 
     def act(self, state):
+        print("test", self.epsilon)
         if np.random.rand() <= self.epsilon:
             return random.randrange(self.action_space)
+        print("testtttt", state.shape)
         act_values = self.model.predict(state)
         return np.argmax(act_values[0])
 
@@ -49,9 +52,13 @@ class DQNAgent:
         for state, action, reward, next_state, done in minibatch:
             target = reward
             if not done:
-                target = reward + self.gamma * np.amax(self.model.predict(next_state)[0])
+                print(next_state.shape)
+                target = reward + self.gamma * np.amax(self.model.predict(nnext_state)[0])
+            print("test", state.shape)
             target_f = self.model.predict(state)
-            target_f[0][action] = target
+            print(int(action))
+            print(target_f.shape)
+            target_f[0][int(action)] = target
             self.model.fit(state, target_f, epochs=1, verbose=0)
         if self.epsilon > self.epsilon_min:
             self.epsilon *= self.epsilon_decay
@@ -63,12 +70,13 @@ class DQNAgent:
         self.model.save_weights(name)
 
 action_space = HAND_SIZE
-state_shape = HAND_SIZE + PLAYER_COUNT*HAND_SIZE + 3
+state_shape = (1, HAND_SIZE + PLAYER_COUNT*HAND_SIZE + 3)
 agent = DQNAgent(state_shape, action_space)
 batch_size = 32
 reward = np.zeros(PLAYER_COUNT)
 last_action = np.zeros(PLAYER_COUNT)
-state = {"hand", "played cards", "own_position", "beginning position", "sich idx"}*PLAYER_COUNT*2
+state = np.zeros((PLAYER_COUNT*2, 1, state_shape[1]))
+print(agent.model.summary())
 
 def play_card(hand, played_cards, own_position, beginning_position, stich_idx):
     """
@@ -85,7 +93,7 @@ def play_card(hand, played_cards, own_position, beginning_position, stich_idx):
     played_card_idx = int(input("What card do you wanna play (index)?"))
     '''
 
-    state = {"hand": hand, "played cards": played_cards, "own_position": own_position, "beginning position": beginning_position, "sich idx": stich_idx}
+    state = np.expand_dims(np.concatenate((hand, played_cards, own_position, beginning_position, stich_idx), axis=None), axis=0)
     played_card_idx = agent.act(state)
 
 
@@ -118,19 +126,23 @@ def game(deck):
                 if alive_players[playing_player_idx] == 1:
                     current_hand = hand[playing_player_idx]
 
-                    new_state = {"hand": current_hand, "played cards": played_cards, "own_position": own_position, 
-                                 "beginning position": beginning_position, "sich idx": stich_idx}
+                    new_state = np.expand_dims(np.concatenate((current_hand, played_cards, player_idx, last_won_position, stich_idx), axis=None), axis=0)
+                    #print(new_state.shape)
 
                     if stich_idx != 0:
-                        agent.remember(state[playing_player_idx], last_action[playing_player_idx], reward[playing_player_idx], new_state)
-                        if len(agent.memory) > batch_size:
-                            agent.replay(bath_size)
+                        #print(state[playing_player_idx].shape)
 
+                        agent.remember(state[playing_player_idx], last_action[playing_player_idx], reward[playing_player_idx], new_state, False)
+                        if len(agent.memory) > batch_size:
+                            agent.replay(batch_size)
+                    print("testtt")
                     action = agent.act(new_state)
                     temp = np.array(current_hand)
                     np.place(temp, temp == 0, 100)
-                    if ((not beginning_position == own_position) and current_hand[action] < played_cards[stich_idx].max()) or current_hand[action] == 0:
+                    if ((not last_won_position == playing_player_idx) and current_hand[action] < played_cards[stich_idx].max()) or current_hand[action] == 0:
                         played_card_idx = np.argmin(temp)
+                    else:
+                        played_card_idx = action
 
                     last_action[playing_player_idx] = action
                     played_cards[stich_idx, playing_player_idx] = current_hand[played_card_idx]
@@ -138,6 +150,8 @@ def game(deck):
 
                     reward[playing_player_idx] = 0
                     if stich_idx != HAND_SIZE-1:
+                        print(new_state.shape)
+                        print(state.shape)
                         state[playing_player_idx] = new_state
                     else:
                         state[playing_player_idx + PLAYER_COUNT] = state[playing_player_idx]
@@ -162,14 +176,18 @@ def game(deck):
 
         new_alive_players = player_points < 21
         for i in range(PLAYER_COUNT):
-            if new_alive_player[i] - alive_players[i] == -1:
+            print(new_alive_players, alive_players)
+            if not new_alive_players[i] and alive_players[i]:
                 reward[i] += -10
-            agent.remember(state[i + PLAYER_COUNT], action[i], reward[i], state[i])
+                done = True
+            else:
+                done = False
+            agent.remember(state[i + PLAYER_COUNT], last_action[i], reward[i], state[i], done)
         alive_players = new_alive_players
 
     player_won_idx = np.argmax(alive_players)
     reward[player_won_idx] += 100
-    agent.remember(state[player_won_idx + PLAYER_COUNT], action[player_won_idx], reward[player_won_idx], state[player_won_idx])
+    agent.remember(state[player_won_idx + PLAYER_COUNT], last_action[player_won_idx], reward[player_won_idx], state[player_won_idx], True)
 
     
 
